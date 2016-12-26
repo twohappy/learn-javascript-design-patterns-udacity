e583ee9bae91b763a2a8eb91b4d096b7b569cc71
twohappy <autumnyout@gmail.com>
2016-12-26 11:33:34 +0800

    模仿...

--- cat-clicker/app.js
+++ cat-clicker/app.js
@@ -1,138 +1,137 @@
 /**
  * Created by twohappy on 2016/12/20.
  */
-//课程里面的代码看了以后,这版程序写的还是有点overplay.
-;(function(){
-    var cats = [
-        {
+//课程里面的代码看了以后,这版程序写的还是有点over.
+//重写
+var model = {
+    currentCat:null,
+    catList: [ {
             name: 'javascript',
-            url: 'img/kitty.jpg'
+            url: 'img/kitty.jpg',
+            click: 0
         },
         {
             name: 'python',
-            url: 'img\/kitty2.jpg'
+            url: 'img/kitty2.jpg',
+            click: 0
         },
         {
             name: 'jQuery',
-            url: 'img\/kitty3.jpg'
+            url: 'img/kitty3.jpg',
+            click: 0
         },
         {
             name: 'HTML',
-            url: 'img\/kitty4.jpg'
+            url: 'img/kitty4.jpg',
+            click: 0
         },
         {
             name: 'CSS',
-            url: 'img\/kitty5.jpg'
-        }];
+            url: 'img/kitty5.jpg',
+            click: 0
+        }]
+};
 
-    var model = {
-        catList: [],
-        addCat: function (name, url) {
-            this.catList.push({
-                name: name,
-                url: url,
-                clickTimes: 0
-            })
-        },
-        init: function (cats) {
-            cats.forEach(function (cur, index) {
-                var cat = {
-                    name: cur.name,
-                    url: cur.url,
-                    clickTimes: 0,
-                    id: index
-                };
-                model.catList.push(cat)
-            });
-        }
-    };
+var octopus = {
+    getCalList:function () {
+        return model.catList;
+    },
+    init:function () {
+        model.currentCat = this.getCalList()[0];
+        listView.init();
+        catView.init();
+        modifyView.init();
+    },
+    setCurrentCat:function (cat) {
+        model.currentCat = cat;
+    },
+    addClick:function () {
+        model.currentCat.click += 1;
+        catView.render();
+    },
+    getCurrentCat:function () {
+        return model.currentCat;
+    },
+    modifyCurrent:function (name,url) {
+        var current = model.currentCat;
+        current.name = name;
+        current.url = url;
+        current.click = 0;
+    }
+};
 
-    var octopus = {
-        getCatList:function () {
-            return model.catList;
-        },
-        getCat:function (num) {
-            return model.catList[num];
-        },
-        currentCat:null,
-        getCatByName:function (name) {
-            var hehe = null;
-            model.catList.forEach(function (cur) {
-                if(cur.name==name){
-                    hehe = cur;
+
+var listView = {
+    init: function () {
+        this.listElem = document.querySelector('#catList');
+        this.render();
+    },
+    render:function () {
+        this.listElem.innerHTML = '';
+        var cats = octopus.getCalList();
+        for(var i=0,l=cats.length;i<l;i++){
+            var cat = cats[i];
+            var ele = document.createElement('li');
+            ele.innerHTML = cat.name;
+            ele.addEventListener('click',(function (catCopy) {
+                return function () {
+                    octopus.setCurrentCat(catCopy);
+                    catView.render();
                 }
-            });
-            return hehe?hehe:false;
-        },
-        addClick:function () {
-            this.currentCat.clickTimes += 1;
-        },
-        changeCat:function (catName) {
-            if(catName === this.currentCat.name){
-
-            }else{
-                this.currentCat = this.getCatByName(catName);
-                catView.render(this.currentCat);
-            }
-
-        },
-        init:function (listEl,catEl) {
-            model.init(cats);
-            this.currentCat = this.getCat(0);
-            listView.init(listEl);
-            catView.init(catEl);
-        }
-    };
-
-
-    var listView = {
-        init: function (el) {
-            this.catList = $(el);
-            var htmlStr = '';
-            octopus.getCatList().forEach(function (cur) {
-                htmlStr += viewTemplate.catList.replace('{{url}}','"'+cur.url+'"').replace('{{catName}}',cur.name);
-            });
-            this.catList.html(htmlStr);
-            this.catList.click(function (e) {
-                console.log('list clicked!');
-                var catName ='';
-                if($(e.target).parent().children('p')){
-                    catName = $(e.target).parent().children('p').html()
-                }
-                octopus.changeCat(catName);
-            });
-        }
-    };
-
-    var catView = {
-        init:function (el) {
-            this.catShow = $(el);
-
-            this.render(octopus.currentCat);
-            this.catShow.click(function () {
-                console.log('cat clicked!');
-                octopus.addClick();
-                catView.render(octopus.currentCat);
-            })
-        },
-        render:function (cat) {
-            var htmlStr = '';
-            htmlStr = viewTemplate.cat
-                .replace('{{catName}}',cat.name)
-                .replace('{{url}}','"'+cat.url+'"')
-                .replace('{{clickTime}}',cat.clickTimes.toString());
-            this.catShow.html(htmlStr);
+            })(cat));
+            this.listElem.appendChild(ele);
         }
     }
+};
 
-    var viewTemplate = {
-        cat: '<div class="cat">' +
-        '<div>Cat Name: {{catName}}</div>' +
-        '<img src={{url}}>' +
-        '<span>Click times: {{clickTime}}</span>' +
-        '</div>',
-        catList: '<li><img src={{url}}><p>{{catName}}</p></li>'
+var catView = {
+    init: function () {
+        this.cat = document.querySelector('#cat');
+        this.catName = document.querySelector('#cat-name');
+        this.catCount = document.querySelector('#cat-count');
+        this.catImg = document.querySelector('#cat-img');
+        this.render(octopus.getCurrentCat());
+        this.cat.addEventListener('click',function () {
+            octopus.addClick();
+        })
+    },
+    render: function () {
+        var cat = octopus.getCurrentCat();
+        this.catName.textContent = cat.name;
+        this.catImg.src = cat.url;
+        this.catCount.textContent = cat.click;
     }
+}
 
-    octopus.init('#catList','#catShow')
-})();
+
+var modifyView = {
+    init:function () {
+        this.newName = document.querySelector('#newName');
+        this.newUrl = document.querySelector('#newUrl');
+        this.ok = document.querySelector('#ok');
+        this.cancel = document.querySelector('#cancel');
+        this.mBtn = document.querySelector('#mBtn');
+        this.area = document.querySelector('#modify');
+        $(this.area).hide();
+        this.mBtn.addEventListener('click',function () {
+            $(modifyView.area).toggle();
+        });
+        this.ok.addEventListener('click',function () {
+            var catName = modifyView.newName.value;
+            var catUrl = modifyView.newUrl.value;
+            octopus.modifyCurrent(catName,catUrl);
+            catView.render();
+            listView.render();
+            modifyView.newName.value = '';
+            modifyView.newUrl.value = '';
+            $(modifyView.area).toggle();
+        });
+        this.cancel.addEventListener('click',function () {
+            modifyView.newName.value = '';
+            modifyView.newUrl.value = '';
+            $(modifyView.area).toggle();
+        })
+    }
+}
+
+octopus.init();
